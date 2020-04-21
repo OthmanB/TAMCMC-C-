8e7c84a551bd56de02bbce9276bb47dcc4ff1f56
Othman Benomar <othman.benomar@gmail.com>
2020-04-21 11:13:30 +0900

    Update version.h

--- tamcmc/headers/version.h
+++ tamcmc/headers/version.h
@@ -9,13 +9,13 @@ void writeversion();
 #define APP_ID        "TAMCMC"
 #define APP_NAME      "CPPTAMCMC"
 #define APP_DESC      "MCMC for Asteroseismology"
-#define APP_VERSION   "1.4.21-dev"
+#define APP_VERSION   "1.4.23-dev"
 #define APP_DOMAIN    ""
 #define APP_SITE      ""
 #define APP_COPYRIGHT "Othman Benomar"
 
 #define APP_VER_MAJOR  1
-#define APP_VER_MINOR  4.21
+#define APP_VER_MINOR  4.23
 //#define APP_VER_BUILD  0
 //#define APP_VER_REV    0
 
